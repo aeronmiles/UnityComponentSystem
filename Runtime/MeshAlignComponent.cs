7a0893d2b8d5191d56e392841d9a1a62011db44e
Aeron Miles <aeron.miles@flipsidegroup.com>
2024-08-07 13:46:18 +0100

    Update namespace

--- Runtime/MeshAlignComponent.cs
+++ Runtime/MeshAlignComponent.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using AM.Unity.Component.System;
 using UnityEngine;
 
-[DisallowMultipleComponent]
-public class MeshAlignComponent : EntityComponent
+namespace AM.Unity.Component.System
 {
+  [DisallowMultipleComponent]
+  public class MeshAlignComponent : EntityComponent
+  {
     public MeshFilter MeshFilter;
     public Transform AlignTransform;
     public bool AlignToNormal = false;
@@ -14,32 +16,33 @@ public class MeshAlignComponent : EntityComponent
     Transform m_Transform;
     private new void Awake()
     {
-        base.Awake();
-        m_Transform = transform;
+      base.Awake();
+      m_Transform = transform;
     }
 
     Vector3[] m_Positions;
     Vector3[] m_Normals;
     public void Update()
     {
-        int vCount = VertexIndicies.Count;
-        if (MeshFilter == null || vCount == 0) return;
+      int vCount = VertexIndicies.Count;
+      if (MeshFilter == null || vCount == 0) return;
 
-        Vector3 pos = Vector3.zero;
-        Vector3 normal = Vector3.zero;
-        Matrix4x4 localToWorld = MeshFilter.transform.localToWorldMatrix;
-        m_Positions = MeshFilter.mesh.vertices;
-        m_Normals = MeshFilter.mesh.normals;
-        foreach (var i in VertexIndicies)
-        {
-            pos += localToWorld.MultiplyPoint3x4(m_Positions[i]);
-            normal += localToWorld.MultiplyVector(m_Normals[i]);
-        }
+      Vector3 pos = Vector3.zero;
+      Vector3 normal = Vector3.zero;
+      Matrix4x4 localToWorld = MeshFilter.transform.localToWorldMatrix;
+      m_Positions = MeshFilter.mesh.vertices;
+      m_Normals = MeshFilter.mesh.normals;
+      foreach (var i in VertexIndicies)
+      {
+        pos += localToWorld.MultiplyPoint3x4(m_Positions[i]);
+        normal += localToWorld.MultiplyVector(m_Normals[i]);
+      }
 
-        m_Transform.position = pos / vCount;
-        if (AlignTransform != null)
-            m_Transform.rotation = AlignTransform.rotation;
-        else if (AlignToNormal)
-            m_Transform.rotation = Quaternion.FromToRotation(Vector3.up, normal / vCount);
+      m_Transform.position = pos / vCount;
+      if (AlignTransform != null)
+        m_Transform.rotation = AlignTransform.rotation;
+      else if (AlignToNormal)
+        m_Transform.rotation = Quaternion.FromToRotation(Vector3.up, normal / vCount);
     }
+  }
 }
